ac073e09401ae95643bae727e287367827f194cc
rahuldogney02 <rahuldogney02@gmail.com>
2025-07-17 16:36:50 +0000

    using jest.fn() for mock our Mongoose

--- TODO-TDD/tests/unit/todo.controller.test.js
+++ TODO-TDD/tests/unit/todo.controller.test.js
@@ -1,7 +1,16 @@
 const TodoController = require("../../controllers/todo.controller");
+const TodoModel = require("../../model/todo.model");
+
+// mock on TodoModel
+TodoModel.create = jest.fn();
+
 
 describe("TodoController.createTodo",()=>{
     it("should have a creatTodo function", ()=>{
         expect(typeof TodoController.createTodo).toBe("function");
+    })
+    it("should call TodoModel.create",()=>{
+        TodoController.createTodo();
+        expect(TodoModel.create).toBeCalled();
     })
 })
